4abc7ec8c3356e3eb3a4320f010211a5a8b78bd1
KennethMokhethi <kennethmahlatsemokhethi@gmail.com>
2020-01-09 10:28:04 +0200

    Created the main ,letterstomorse and morsecodetoletters classes

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+  private static String letterstoMorse(String word)
+  {
+      return "Hell";
+  }
+
+  private static String morseCodetoletters(String word)
+  {
+      return "Needs code";
+  }
+
+
+    public static void main(String[] args)
+    {
+
+    }
+}
