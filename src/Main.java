1bcd9ff326571393aaec9365eef9269554acf2a1
KennethMokhethi <kennethmahlatsemokhethi@gmail.com>
2020-01-09 16:00:12 +0200

    Implemented morsecodetoletters function:Not complete

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,29 @@
-
-
 import java.util.Scanner;
 
 public class Main {
 
   //public static char[] letters ={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+  // new String[]{"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
+          //        "p","q","r","s","t","u","v","w","x","y","z"};
 
-
-
-
-          //new String[]{"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
-          //                                                "p","q","r","s","t","u","v","w","x","y","z"};
+  public static String[] letters =new String[]{"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
+                                              "p","q","r","s","t","u","v","w","x","y","z"};
+  //String array containing the morse characyers
   public static String[] morse = new String[]{ ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",
           ".---", "-.-", ".-..", "--", "-.", "---", ".---.", "--.-", ".-.",
-          "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----",
+          "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+
+         /* , ".----",
           "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.",
-          "-----", "--..--", ".-.-.-", "..--.." };
+          "-----", "--..--", ".-.-.-", "..--.." };*/
 
-
-
-  private static String letterstoMorse(String word)
+  //Function that convert a word to morse code
+  private static String lettersToMorseCode(String word)
   {
 
-     word = word.toLowerCase();
-    String morseletter=" ";
+    word = word.toLowerCase();
+    String morseletter = " ";
+    //Search for whitespace and replace with a forward slash
     if(word.matches("^\\s*$"))
     {
       word = word.replace("/"," ");
@@ -32,30 +32,50 @@ public class Main {
     //For each loop
     for(char i: word.toCharArray())
     {
-   //   int index_of_char_in_word=findindex(i);
-      if(i==32){
-
-        morseletter+=" "+"/"+" ";
+      if(i == 32){ //Number 32 represent a forward slash in ascii table
+        morseletter+=" " + "/" + " ";
       }else{
-        morseletter+=morse[i-'a']+" ";
+        morseletter += morse[i - 'a'] + " ";
       }
     }
       return morseletter;
   }
 
-  private static String morseCodetoletters(String word)
+  private static String morseCodetoLetters(String word)
   {
-      return "Needs code";
+   String[] words = word.split(" ");
+   String sentence = " ";
+   for(int i =0 ;i < words.length; i++)
+   {
+     for(int k = 0;k < morse.length; k++)
+     {
+       if(words[i]==morse[k])
+       {
+         sentence+=letters[k];
+       }else if((words[i]=="/") || (words[i]=="")){
+         sentence += " ";
+       }
+     }
+   }
+
+      return sentence;
   }
 
 
     public static void main(String[] args)
     {
-      Scanner scan = new Scanner(System.in);
+      //Testing function that transform a word to morse code
+     /* Scanner scan = new Scanner(System.in);
       System.out.println("Enter word");
       String word=scan.nextLine();
 
-      String res=letterstoMorse(word);
-      System.out.println(res);
+      String morse_code=lettersToMorseCode(word);
+      System.out.println(morse_code);*/
+
+      //Testing a function that transform a morse code to a sentence
+      //System.out.println("Enter the morse code");
+      //String strMorse_code=scan.nextLine();
+      String sentence =morseCodetoLetters("-... .- -... .-  / .-");
+      System.out.println(sentence);
     }
 }
